e40bcc85fee21993d3a2b72da989b23972d58610
*QMaWtX6qMd <poulenc51@gmail.com>
2024-03-20 16:10:50 +0900

    Initial commit

---
+++ src/main/kotlin/com/quocard/bookshelf/BookshelfApplication.kt
@@ -0,0 +1,11 @@
+package com.quocard.bookshelf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookshelfApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookshelfApplication>(*args)
+}
---
+++ src/test/kotlin/com/quocard/bookshelf/BookshelfApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.quocard.bookshelf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookshelfApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
