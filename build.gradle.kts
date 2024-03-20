b6a8650d282170bd19478f85c2c44139dbd9c154
*QMaWtX6qMd <poulenc51@gmail.com>
2024-03-20 23:19:07 +0900

    jooqの追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import nu.studer.gradle.jooq.JooqExtension
+import nu.studer.gradle.jooq.JooqEdition
 
 plugins {
 	id("org.springframework.boot") version "3.2.3"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("nu.studer.jooq") version "9.0"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
 }
@@ -22,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jooq:jooq")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -36,4 +40,37 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+configure<nu.studer.gradle.jooq.JooqExtension> {
+	version.set("3.13.1") // 使用するjOOQのライブラリのバージョン
+	edition.set(JooqEdition.OSS)
+	configurations {
+		create("main") {  // ここでは「sample("main") {」の代わりに「create("main") {」を使用します
+			jooqConfiguration.apply {
+				jdbc.apply {
+					driver = "org.h2.Driver"
+					url = "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE"
+					user = "sa"
+					password = ""
+				}
+				generator.apply {
+					name = "org.jooq.codegen.DefaultGenerator"
+					database.apply {
+						name = "org.jooq.meta.h2.H2Database"
+						includes = ".*"
+						inputSchema = "PUBLIC"
+					}
+					generate.apply {
+						isDeprecated = false
+						isTables = true
+					}
+					target.apply {
+						packageName = "com.api.bookshelf.jooq"
+						directory = "src/main/java"
+					}
+				}
+			}
+		}
+	}
 }
