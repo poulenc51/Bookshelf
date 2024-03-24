27e6937bb495b6ad519a62ae6dc74437d74ae53a
*QMaWtX6qMd <poulenc51@gmail.com>
2024-03-24 22:55:52 +0900

    書籍初期データ作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
-MERGE INTO TODO (id, title, description, is_completed, created_at, updated_at)
-KEY(id)
-VALUES
-(1, '買い物リスト', '牛乳, パン, りんご', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(2, '郵便局に行く', '郵便局で小包を受け取る', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(3, '家賃の支払い', '家賃を振り込む', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(4, 'ジムに行く', '週に2回筋トレ&カーディオ', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(5, '映画を観る', '新作の映画を観に行く', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-WHEN NOT MATCHED THEN INSERT;
+MERGE INTO BOOK USING (VALUES
+    (1, 'ノルウェイの森', 1, '1987-9-4', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, '吾輩は猫である', 2, '1905-1-1', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3, '羅生門', 3, '1971-3-5', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (4, 'Harry Potter and the Philosopher’s Stone', 4, '1997-6-26', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (5, '罪と罰', 5, '1866-12-1', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+) AS source (id, title, author_id, publication_date, is_deleted, created_at, updated_at)
+ON BOOK.id = source.id
+WHEN NOT MATCHED THEN
+    INSERT (id, title, author_id, publication_date, is_deleted, created_at, updated_at)
+    VALUES (source.id, source.title, source.author_id, source.publication_date, source.is_deleted, source.created_at, source.updated_at);
