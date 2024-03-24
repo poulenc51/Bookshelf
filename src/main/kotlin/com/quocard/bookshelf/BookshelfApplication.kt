53710e715330843489e121e028e2a14eb6bc08d8
Yuto Murakami <41503882+poulenc51@users.noreply.github.com>
2024-03-24 21:33:27 +0900

    Revert "環境構築 (#1)"
    This reverts commit 7949593d238aab7259efbdc7f474fb9d42972778.

--- src/main/kotlin/com/quocard/bookshelf/BookshelfApplication.kt
+++ src/main/kotlin/com/quocard/bookshelf/BookshelfApplication.kt
@@ -1,4 +1,4 @@
-package com.api.bookshelf
+package com.quocard.bookshelf
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- src/test/kotlin/com/quocard/bookshelf/BookshelfApplicationTests.kt
+++ src/test/kotlin/com/quocard/bookshelf/BookshelfApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.api.bookshelf
+package com.quocard.bookshelf
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
